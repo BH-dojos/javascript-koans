49e3453b2375e30e70f9d364c34099ace9165783
Bruno Trecenti <btrecent@thoughtworks.com>
2015-05-07 20:19:44 -0300

    understaing about this keyword on objects

--- koans/AboutObjects.js
+++ koans/AboutObjects.js
@@ -44,8 +44,8 @@ describe("About Objects", function () {
       }
     };
 
-    expect(currentYear).toBe(FILL_ME_IN);
-    expect(megalomaniac.calculateAge()).toBe(FILL_ME_IN);
+    expect(currentYear).toBe(2015);
+    expect(megalomaniac.calculateAge()).toBe(2015 - 1970);
   });
 
   describe("'in' keyword", function () {
